d8d12bc3f6a168929f00b6607e75f0ea3955edd6
clodoan <angrigianicr@gmail.com>
2023-05-07 22:02:11 -0400

    fix branch build

--- pages/home/components/timeline/components/row/row.tsx
+++ pages/home/components/timeline/components/row/row.tsx
@@ -1,9 +1,12 @@
+import Typography from '@/components/typography/typography';
+import { spacing } from '@/styles';
 import React from 'react';
+import styled, { css } from 'styled-components';
 
 type RowProps = {
   type: 'work' | 'education';
-  title: string;
   position?: string;
+  degree?: string;
   institution: string;
   dateStart: string;
   dateEnd: string;
@@ -11,19 +14,57 @@ type RowProps = {
 
 const Row = ({
   type,
-  title,
   position,
+  degree,
   institution,
   dateStart,
   dateEnd,
 }: RowProps) => {
-  return (
-    <>
-      <Title>{title}</Title>
-      <Position>{position}</Position>
-      <Institution>{institution}</Institution>
-      <DateStart>{dateStart}</DateStart>
-      <DateEnd>{dateEnd}</DateEnd>
-    </>
+  return type === 'work' ? (
+    <Container>
+      <Institution variant="body-2">{institution}</Institution>
+      <Right>
+        <Position variant="label-2">{position}</Position>
+        <Date>
+          <DateStart variant="label-2">{dateStart}</DateStart>
+          <DateEnd variant="label-2">{dateEnd}</DateEnd>
+        </Date>
+      </Right>
+    </Container>
+  ) : (
+    <></>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: ${spacing[2]};
+`;
+
+const Title = styled(Typography)``;
+
+const Position = styled(Typography)``;
+
+const Institution = styled(Typography)``;
+
+const DateStart = styled(Typography)``;
+
+const DateEnd = styled(Typography)``;
+
+const Date = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  gap: ${spacing[1]};
+`;
+
+const Right = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export default Row;
