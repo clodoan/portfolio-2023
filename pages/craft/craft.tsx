dc786dda02ed52251141689acb29344a84319579
claudio angrigiani <claudio@onefootprint.com>
2024-04-06 19:41:45 -0400

    add

--- pages/craft/craft.tsx
+++ pages/craft/craft.tsx
@@ -1,3 +1,5 @@
+import { Typography } from '@/components';
+import { media } from '@/styles/media';
 import MuxPlayer from '@mux/mux-player-react';
 import React from 'react';
 import styled, { css } from 'styled-components';
@@ -8,57 +10,78 @@ const Craft = () => {
   return (
     <Container>
       {data.map((item) => (
-        <Frame>
-          <VideoContainer>
-            <MuxPlayer
-              key={item.id}
-              playbackId={item.playbackId}
-              metadata={{
-                video_title: 'Dialog overflow',
-              }}
-              autoPlay
-              loop
-              muted
-              playsInline
-              style={{
-                pointerEvents: 'none',
-              }}
-            />
-          </VideoContainer>
-        </Frame>
+        <VideoContainer key={item.id}>
+          <CaptionContainer>
+            <Typography variant="body-2">{item.title}</Typography>
+          </CaptionContainer>
+          <MuxPlayer
+            key={item.id}
+            playbackId={item.playbackId}
+            metadata={{
+              video_title: 'Dialog overflow',
+            }}
+            autoPlay
+            loop
+            muted
+            playsInline
+            style={{
+              pointerEvents: 'none',
+              height: '100%',
+              width: '100%',
+            }}
+          />
+        </VideoContainer>
       ))}
     </Container>
   );
 };
 
+const VideoContainer = styled.div<{ key: string; children: React.ReactNode }>`
+  border-radius: 8px;
+  width: fit-content;
+  height: auto;
+  overflow: hidden;
+  position: relative;
+  isolation: isolate;
+
+  mux-player {
+    --media-object-fit: cover;
+    --media-object-position: center;
+    --controls: none;
+  }
+`;
+
+const CaptionContainer = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    z-index: 1;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: linear-gradient(to top, black 0%, transparent 100%);
+    padding: ${theme.spacing[4]} ${theme.spacing[3]};
+
+    p {
+      color: #fff;
+    }
+  `}
+`;
+
 const Container = styled.div`
   ${({ theme }) => css`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
     grid-gap: ${theme.spacing[2]};
-  `}
-`;
+    padding: ${theme.spacing[2]};
+    grid-template-columns: repeat(1, 1fr);
+    grid-template-rows: masonry;
 
-const VideoContainer = styled.div`
-  ${({ theme }) => css`
-    width: 100%;
-    aspect-ratio: 16/9;
-    border-radius: 7px;
-    width: fit-content;
-    height: fit-content;
-    overflow: hidden;
-  `}
-`;
+    @media (${media.desktop}) {
+      grid-template-columns: repeat(2, 1fr);
+    }
 
-const Frame = styled.div`
-  ${({ theme }) => css`
-    padding: ${theme.spacing[1]};
-    background-color: ${theme.colors.background.secondary};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    border-radius: 9px;
+    @media (${media.desktopL}) {
+      grid-template-columns: repeat(3, 1fr);
+    }
   `}
 `;
 
